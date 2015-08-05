2f20697dcd9a8d1b11c1d2e139589dbd19ab06c8
Josh <jostricklin@gmail.com>
2015-08-05 15:40:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamblog::Application.config.secret_key_base = '59b47cbea8540c4c781a6ed9bc0ffa4038b0f6cec4b04932b10b6d00f2fdd5c1c9f255b736de3bfdbcc8777b158d5791ecf4ecaef0455ce6058486092bd8d714'
